095add7fe7981e0f6da31fe39de8b33391a39858
amainkar <atharva.mainkar@gmail.com>
2021-02-20 02:18:48 -0600

    Got some scrapping stuff working

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import webbrowser
+
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://www.depts.ttu.edu/communications/emergency/coronavirus/")
+
+tech_COVID_Data = driver.find_element_by_xpath("//table")
+print(tech_COVID_Data.text + "\n")
+driver.get("https://www.depts.ttu.edu/recsports/facilities/hours.php")
+TTU_Rec_Hours =driver.find_element_by_xpath("//*[@id='calendar']")
+print(TTU_Rec_Hours.text)
+driver.close()
